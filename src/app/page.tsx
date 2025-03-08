cf2ac518296ae2ed6e7005fa87912c5f7683fe71
hiltonoliveir4 <hiltonoliveira@inforgeneses.com.br>
2025-03-08 11:30:05 -0300

    projeto

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,100 +2,71 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+    <main className="flex flex-col snap-y snap-mandatory h-screen w-screen overflow-scroll">
+      <div className="snap-center relative bg-pink-950 flex flex-col items-center justify-center py-16 gap-6 p-4 h-screen">
+        <div className="text-white flex flex-col items-center">
+          <h1 className="text-2xl font-bold">
+            Mauritania
+          </h1>
+          <p className="text-base">Com todo meu amor e carinho <span className="animate-pulse">❤️</span></p>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+        <Image 
+          className="rounded-2xl shadow-2xl w-full"
+          src={"/IMG_6429.jpg"}
+          width={300}
+          height={300}
+          alt="imagem"
+        />
+        <p className="text-white w-full">
+          Meu amor, você é a minha musa fitness! 
+          Minha maior inspiração dentro e fora da academia. 
+          Sua disciplina, e sua dedicação (no Gymrats) me enchem de orgulho todos os dias. 
+          Te admiro demais e sou grato por compartilhar essa vida com você!
+        </p>
+        <Image className="w-full absolute bottom-[-5px] left-0" src={"/wave.png"} width={100} height={100} alt="wave"></Image>
+      </div>
+      <div className="snap-center relative bg-white flex flex-col items-center justify-center py-8 gap-6 p-4 h-screen">    
+        <Image 
+          className="rounded-2xl shadow-2xl w-full"
+          src={"/trabalho.jpg"}
+          width={300}
+          height={300}
+          alt="imagem"
+        />
+        <p className="text-pink-950">
+          Você não é apenas incrível dentro de casa, mas também brilha em tudo que faz! 
+          Sua dedicação, inteligência e talento fazem de você a melhor bibliotecária do mundo. 
+          Tenho orgulho de ver o quanto você se esforça e conquista seus objetivos. 
+        </p>
+      </div>
+      <div className="snap-center relative bg-pink-950 flex flex-col items-center justify-center py-16 gap-6 p-4 h-screen">
+        <Image 
+          className="rounded-2xl shadow-2xl w-full"
+          src={"/IMG_7158.jpg"}
+          width={300}
+          height={300}
+          alt="imagem"
+        />
+        <p className="text-white">
+          Ser seu namorado é o maior presente da minha vida. 
+          Você é mais do que minha companheira, é minha melhor amiga, 
+          meu porto seguro e a pessoa que faz qualquer dia comum se tornar especial. 
+          Seu amor, carinho e paciência* (nem sempre) tornam minha vida completa. 
+          Sou um homem de sorte por ter você ao meu lado!
+        </p>
+        <Image className="w-full absolute top-[-5px] left-0" src={"/wave-reverse.png"} width={100} height={100} alt="wave"></Image>
+        <Image className="w-full absolute bottom-[-5px] left-0" src={"/wave.png"} width={100} height={100} alt="wave"></Image>
+      </div>
+      <div className="snap-center h-screen relative bg-white flex flex-col items-center justify-center pt-10 pb-28 gap-10 p-4">
+        <h2 className="text-5xl font-bold text-center text-pink-950 py-4">Feliz dia das mulheres!</h2>
+        <Image 
+          className="rounded-2xl shadow-2xl w-full"
+          src={"/8374095_114d5.gif"}
+          width={300}
+          height={300}
+          alt="imagem"
+        />
+      </div>
+    </main>
   );
 }
